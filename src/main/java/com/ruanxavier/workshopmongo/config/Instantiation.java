bfc6c9056331e3b560135b48f86329571951f970
Ruan Xavier <ruanxavierdossantos81@gmail.com>
2026-06-09 12:56:04 -0300

    Operação de instanciação no banco de dados para testes

---
+++ src/main/java/com/ruanxavier/workshopmongo/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.ruanxavier.workshopmongo.config;
+
+import com.ruanxavier.workshopmongo.domain.User;
+import com.ruanxavier.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+
+    // Operação de instanciação no banco de dados para testes
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+
+    }
+}
