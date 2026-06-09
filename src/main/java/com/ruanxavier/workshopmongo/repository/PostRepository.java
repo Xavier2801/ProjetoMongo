2e7bb97df2b84fd93c49074d1de212e103ad44d9
Ruan Xavier <ruanxavierdossantos81@gmail.com>
2026-06-09 18:02:55 -0300

    Criando o entity Post com User aninhado

---
+++ src/main/java/com/ruanxavier/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.ruanxavier.workshopmongo.repository;
+
+
+import com.ruanxavier.workshopmongo.domain.Post;
+import com.ruanxavier.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+
+
+}
