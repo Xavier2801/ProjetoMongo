7a5e42a5fe80da10a3565d640143fc7f87434659
Ruan Xavier <ruanxavierdossantos81@gmail.com>
2026-06-10 13:21:49 -0300

    Consulta simples com @query

--- src/main/java/com/ruanxavier/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ruanxavier/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.ruanxavier.workshopmongo.repository;
 import com.ruanxavier.workshopmongo.domain.Post;
 import com.ruanxavier.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,10 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
+
 
 }
