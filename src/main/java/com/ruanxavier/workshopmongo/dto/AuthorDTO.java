973f495c7f35dc3096ab82929c46d5e6bc152cb3
Ruan Xavier <ruanxavierdossantos81@gmail.com>
2026-06-10 11:57:12 -0300

    Projeção de dados do autor com DTO

---
+++ src/main/java/com/ruanxavier/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,20 @@
+package com.ruanxavier.workshopmongo.dto;
+
+import com.ruanxavier.workshopmongo.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {}
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+}
