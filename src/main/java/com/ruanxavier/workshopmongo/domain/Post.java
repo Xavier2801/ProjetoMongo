3efd120ef3a7b69238594357800366d75ab8e4f1
Ruan Xavier <ruanxavierdossantos81@gmail.com>
2026-06-10 12:48:13 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/ruanxavier/workshopmongo/domain/Post.java
+++ src/main/java/com/ruanxavier/workshopmongo/domain/Post.java
@@ -1,11 +1,15 @@
 package com.ruanxavier.workshopmongo.domain;
 
 import com.ruanxavier.workshopmongo.dto.AuthorDTO;
+import com.ruanxavier.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.xml.stream.events.Comment;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +22,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     // Construtores
     public Post(){}
@@ -72,6 +77,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     // Hashcode
 
 
@@ -87,6 +100,7 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 
 
 }
---
+++ src/main/java/com/ruanxavier/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,49 @@
+package com.ruanxavier.workshopmongo.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CommentDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private String text;
+    private Date date;
+    private AuthorDTO author;
+
+    // Construtores
+    public CommentDTO(){}
+
+    public CommentDTO(String text, Date date, AuthorDTO author) {
+        this.text = text;
+        this.date = date;
+        this.author = author;
+    }
+
+    // Getters and setters
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDTO author) {
+        this.author = author;
+    }
+}
